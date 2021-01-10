482131bb08275498a8e74a0aadbe0cdaab23a8bc
Maythee Anegboonlap <null@llun.me>
2021-01-10 19:37:38 +0800

    Show diff file first with status

--- repository.js
+++ repository.js
@@ -47,9 +47,10 @@ async function publish() {
     const core = require('@actions/core')
     run('git config --global user.email bot@llun.dev')
     run('git config --global user.name "Feed bots"')
-    run('git add -f contents')
-    run('git commit -m "update feeds contents"')
-    run('git log')
+    run('git status')
+    // run('git add -f contents')
+    // run('git commit -m "update feeds contents"')
+    // run('git log')
   }
 }
 exports.publish = publish
