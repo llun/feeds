09ddebae359cb139e00bd9103472ef87e1e2d49f
Maythee Anegboonlap <null@llun.me>
2021-01-10 19:35:46 +0800

    Fix git add

--- repository.js
+++ repository.js
@@ -47,9 +47,9 @@ async function publish() {
     const core = require('@actions/core')
     run('git config --global user.email bot@llun.dev')
     run('git config --global user.name "Feed bots"')
-    run('git add contents')
+    run('git add -f contents')
     run('git commit -m "update feeds contents"')
-    run('git logs')
+    run('git log')
   }
 }
 exports.publish = publish
