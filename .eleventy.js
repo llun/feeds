fba4769afecf2ec1660000d174eb9fead671fa6e
Maythee Anegboonlap <null@llun.me>
2021-01-10 22:18:32 +0800

    Try remove output config

--- .eleventy.js
+++ .eleventy.js
@@ -34,8 +34,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: 'pages',
       includes: '_includes',
-      data: '_data',
-      output: '_site'
+      data: '_data'
     }
   }
 }
