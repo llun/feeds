747a237a25af650c49ccee949d2cf9bf3e89449f
Maythee Anegboonlap <null@llun.me>
2021-01-10 16:13:51 +0800

    Add actions/core

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // @ts-check
 const { spawnSync } = require('child_process')
 
+async function setup() {
+  if (process.env['GITHUB_ACTION'] === 'lluntest-action') {
+    spawnSync('npm', ['install'], {
+      cwd: '/home/runner/work/_actions/llun/test-action/main',
+      stdio: 'inherit'
+    })
+  }
+
+  spawnSync('ls', [], {
+    cwd: process.env['GITHUB_WORKSPACE'],
+    stdio: 'inherit'
+  })
+}
+
 async function run() {
-  spawnSync('npm', ['install'], {
-    cwd: '/home/runner/work/_actions/llun/test-action/main',
-    stdio: 'inherit'
-  })
-  spawnSync('env', [], {
-    cwd: '/home/runner/work/_actions/llun/test-action/main',
-    stdio: 'inherit'
-  })
+  await setup()
 }
 
 run()
