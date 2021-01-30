8cc71bff887f2d5aa2bc3c061c3613fde8b3aab3
Maythee Anegboonlap <null@llun.me>
2021-01-30 17:36:01 +0800

    Add browser history

--- browser/index.tsx
+++ browser/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-
+import BrowserHistory from 'history/browser'
 import type {
   CategoryData,
   SiteEntryData,
@@ -56,90 +56,141 @@ function categoriesClassName(pageState: PageState): string {
   }
 }
 
+type LocationState =
+  | {
+      type: 'selectCategory'
+      category: string
+    }
+  | {
+      type: 'selectSite'
+      siteHash: string
+    }
+  | {
+      type: 'selectEntry'
+      entryHash: string
+    }
+
+function changePage(url: string, state: LocationState) {
+  BrowserHistory.push(url, state)
+}
+
 const Page = () => {
   const [pageState, setPageState] = useState<PageState>('categories')
   const [entries, setEntries] = useState<SiteEntryData[]>([])
   const [entry, setEntry] = useState<EntryData | undefined>()
   const [loadingProgress, setLoadingProgress] = useState<number | null>(null)
 
-  const selectCategory = async (category: string) => {
-    setLoadingProgress(0)
-    const response = await fetchWithProgress(
-      `${github.repository}/data/categories/${category}.json`,
-      async (bytes: number, total: number) => {
-        setLoadingProgress((bytes / total) * 100)
-      }
-    )
-    if (!response.ok) return
+  useEffect(() => {
+    const unlisten = BrowserHistory.listen(async ({ location, action }) => {
+      const state = location.state
+      if (!state) return
+      const locationState = state as LocationState
+      switch (locationState.type) {
+        case 'selectCategory': {
+          const category = locationState.category
+          setLoadingProgress(0)
+          const response = await fetchWithProgress(
+            `${github.repository}/data/categories/${category}.json`,
+            async (bytes: number, total: number) => {
+              setLoadingProgress((bytes / total) * 100)
+            }
+          )
+          if (!response.ok) return
 
-    const json: SiteEntryData[] = JSON.parse(response.text)
-    setLoadingProgress(100)
-    setTimeout(() => {
-      setLoadingProgress(null)
-    }, 100)
-    setPageState('entries')
-    setEntries(json)
-    setEntry(undefined)
-  }
-
-  const selectSite = async (siteHash: string) => {
-    if (siteHash === 'all') {
-      setLoadingProgress(0)
-      const response = await fetchWithProgress(
-        `${github.repository}/data/all.json`,
-        async (bytes: number, total: number) => {
-          setLoadingProgress((bytes / total) * 100)
+          const json: SiteEntryData[] = JSON.parse(response.text)
+          setLoadingProgress(100)
+          setTimeout(() => {
+            setLoadingProgress(null)
+          }, 100)
+          setPageState('entries')
+          setEntries(json)
+          setEntry(undefined)
+          break
         }
-      )
-      if (!response.ok) return
+        case 'selectSite': {
+          const { siteHash } = locationState
+          if (siteHash === 'all') {
+            setLoadingProgress(0)
+            const response = await fetchWithProgress(
+              `${github.repository}/data/all.json`,
+              async (bytes: number, total: number) => {
+                setLoadingProgress((bytes / total) * 100)
+              }
+            )
+            if (!response.ok) return
 
-      const json: SiteEntryData[] = JSON.parse(response.text)
-      setLoadingProgress(100)
-      setTimeout(() => {
-        setLoadingProgress(null)
-      }, 100)
-      setPageState('entries')
-      setEntries(json)
-      setEntry(undefined)
-      return
+            const json: SiteEntryData[] = JSON.parse(response.text)
+            setLoadingProgress(100)
+            setTimeout(() => {
+              setLoadingProgress(null)
+            }, 100)
+            setPageState('entries')
+            setEntries(json)
+            setEntry(undefined)
+            return
+          }
+
+          setLoadingProgress(0)
+          const response = await fetchWithProgress(
+            `${github.repository}/data/sites/${siteHash}.json`,
+            async (bytes: number, total: number) => {
+              setLoadingProgress((bytes / total) * 100)
+            }
+          )
+          if (!response.ok) return
+
+          const json: SiteDataWithEntries = JSON.parse(response.text)
+          setLoadingProgress(100)
+          setTimeout(() => {
+            setLoadingProgress(null)
+          }, 100)
+          setPageState('entries')
+          setEntries(json.entries)
+          setEntry(undefined)
+          break
+        }
+        case 'selectEntry': {
+          const { entryHash } = locationState
+          setLoadingProgress(0)
+          const response = await fetchWithProgress(
+            `${github.repository}/data/entries/${entryHash}.json`,
+            async (bytes: number, total: number) => {
+              setLoadingProgress((bytes / total) * 100)
+            }
+          )
+          if (!response.ok) return
+
+          const json: EntryData = JSON.parse(response.text)
+          setLoadingProgress(100)
+          setTimeout(() => {
+            setLoadingProgress(null)
+          }, 100)
+          setPageState('article')
+          setEntry(json)
+          break
+        }
+      }
+    })
+    return () => {
+      unlisten()
     }
+  })
 
-    setLoadingProgress(0)
-    const response = await fetchWithProgress(
-      `${github.repository}/data/sites/${siteHash}.json`,
-      async (bytes: number, total: number) => {
-        setLoadingProgress((bytes / total) * 100)
-      }
-    )
-    if (!response.ok) return
-
-    const json: SiteDataWithEntries = JSON.parse(response.text)
-    setLoadingProgress(100)
-    setTimeout(() => {
-      setLoadingProgress(null)
-    }, 100)
-    setPageState('entries')
-    setEntries(json.entries)
-    setEntry(undefined)
-  }
-  const selectEntry = async (entryHash: string) => {
-    setLoadingProgress(0)
-    const response = await fetchWithProgress(
-      `${github.repository}/data/entries/${entryHash}.json`,
-      async (bytes: number, total: number) => {
-        setLoadingProgress((bytes / total) * 100)
-      }
-    )
-    if (!response.ok) return
-
-    const json: EntryData = JSON.parse(response.text)
-    setLoadingProgress(100)
-    setTimeout(() => {
-      setLoadingProgress(null)
-    }, 100)
-    setPageState('article')
-    setEntry(json)
-  }
+  const selectCategory = async (category: string) =>
+    changePage(`/categories/${category}`, {
+      type: 'selectCategory',
+      category
+    })
+  const selectSite = async (siteHash: string) =>
+    changePage(`/sites/${siteHash}`, {
+      type: 'selectSite',
+      siteHash
+    })
+  const selectEntry = async (entryHash: string) =>
+    changePage(`/entry/${entryHash}`, {
+      type: 'selectEntry',
+      entryHash
+    })
 
   return (
     <>
