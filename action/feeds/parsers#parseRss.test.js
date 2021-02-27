7a46b1715da88f622294357d81247a847aba474a
Maythee Anegboonlap <null@llun.me>
2021-02-27 11:55:17 +0800

    Add more parser tests

---
+++ action/feeds/parsers#parseRss.test.js
@@ -0,0 +1,32 @@
+// @ts-check
+const test = /** @type {import('ava').TestInterface<{}>} */ (require('ava')
+  .default)
+const sinon = require('sinon')
+const fs = require('fs')
+const path = require('path')
+const { parseXML, parseRss } = require('./parsers')
+
+test('#parseAtom returns site information with empty string for fields that does not have information', async (t) => {
+  const data = fs
+    .readFileSync(path.join(__dirname, 'tests', 'rss1.xml'))
+    .toString('utf8')
+  const xml = await parseXML(data)
+  const site = parseRss(xml)
+  const firstEntry = xml.rss.channel[0].item[0]
+  t.is(site?.entries.length, 10)
+  sinon.assert.match(site, {
+    title: 'icez network',
+    description: 'Technical Blog by icez network',
+    link: 'https://www.icez.net/blog',
+    updatedAt: new Date('2021-02-08T10:05:50Z').getTime(),
+    generator: 'https://wordpress.org/?v=5.3.6',
+    entries: sinon.match.array
+  })
+  sinon.assert.match(site?.entries[0], {
+    title: firstEntry.title.join('').trim(),
+    link: firstEntry.link.join('').trim(),
+    date: new Date('2021-02-08T10:05:48Z').getTime(),
+    content: firstEntry['content:encoded'].join('').trim(),
+    author: 'icez'
+  })
+})
