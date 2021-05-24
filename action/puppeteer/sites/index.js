d0a6d39acded61228eaf4466966e923d0e4c3d9d
Maythee Anegboonlap <null@llun.me>
2021-05-24 21:56:38 +0800

    Use readability to oad bikerumor

--- action/puppeteer/sites/index.js
+++ action/puppeteer/sites/index.js
@@ -6,6 +6,7 @@ const readabilityLoader = require('./readability.loader')
 const defaultSiteLoaders = new Map([
   ['cyclingtips.com', cyclingtipscomLoader],
   ['cheeaun.com', readabilityLoader],
-  ['www.somkiat.cc', readabilityLoader]
+  ['www.somkiat.cc', readabilityLoader],
+  ['bikerumor.com', readabilityLoader]
 ])
 exports.defaultSiteLoaders = defaultSiteLoaders
