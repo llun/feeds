a15d8bdaa6469d98d441d05135be6b095b4223d1
Maythee Anegboonlap <null@llun.me>
2021-12-05 20:57:36 +0800

    Add github configs

--- lib/config.test.ts
+++ lib/config.test.ts
@@ -1,5 +1,23 @@
 import test from 'ava'
+import { getGithubConfigs } from './config'
 
-test('#getGithub', (t) => {
-  t.fail()
+test('#getGithubConfigs', (t) => {
+  t.deepEqual(
+    getGithubConfigs({ githubRootName: '', customDomain: 'feeds.llun.dev' }),
+    {
+      repository: ''
+    }
+  )
+  t.deepEqual(
+    getGithubConfigs({
+      githubRootName: 'octocat/Hello-World',
+      customDomain: 'feeds.llun.dev'
+    }),
+    {
+      repository: ''
+    }
+  )
+  t.deepEqual(getGithubConfigs({ githubRootName: 'octocat/Hello-World' }), {
+    repository: '/Hello-World'
+  })
 })
--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,24 @@
-export const getGithub = () => {}
+interface GithubConfigs {
+  repository: string
+}
+interface GithubConfigParameters {
+  // Github repository path in format 'user/repository'
+  // This provide by `GITHUB_REPOSITORY` in github action
+  githubRootName: string
+  // Optional custom domain name.
+  // Input from Github action configuration `customDomain`
+  // Get this value by `core.getInput('customDomain')`
+  customDomain?: string
+}
+export const getGithubConfigs = ({
+  githubRootName,
+  customDomain
+}: GithubConfigParameters): GithubConfigs => {
+  return {
+    repository:
+      (!customDomain &&
+        githubRootName.split('/').length > 1 &&
+        `/${githubRootName.split('/')[1]}`) ||
+      ''
+  }
+}
