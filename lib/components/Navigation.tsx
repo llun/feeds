4a89c89d0b24812c53f893d60d1f6ae61a11d22c
Maythee Anegboonlap <null@llun.me>
2021-12-07 09:33:33 +0800

    Prepare for SPA

--- lib/components/Navigation.tsx
+++ lib/components/Navigation.tsx
@@ -1,26 +1,39 @@
-import Link from 'next/link'
 import { Fragment } from 'react'
 import { Category, GithubConfigs } from '../data'
 
 interface Props {
   githubConfigs: GithubConfigs
   categories: Category[]
+  selectCategory?: (category: string) => Promise<void>
+  selectSite?: (site: string) => Promise<void>
 }
-const Navigation = ({ githubConfigs, categories }: Props) => {
+const Navigation = ({ categories, selectCategory, selectSite }: Props) => {
   return (
     <aside className="prose max-w-none w-full sm:w-48 xl:w-96 flex-shrink-0 p-6 block">
       <h1>Feeds</h1>
       <h2 className="cursor-pointer">
-        <Link href={`${githubConfigs.repository}/all`}>All sites</Link>
+        <a onClick={() => selectSite && selectSite('all')}>All sites</a>
       </h2>
       {categories.map((category) => (
         <Fragment key={category.name}>
-          <h2 className="cursor-pointer">{category.name}</h2>
+          <h2 className="cursor-pointer">
+            <a onClick={() => selectCategory && selectCategory(category.name)}>
+              {category.name}
+            </a>
+          </h2>
           <ul>
-            <li>All sites</li>
+            <li>
+              <a
+                onClick={() => selectCategory && selectCategory(category.name)}
+              >
+                All sites
+              </a>
+            </li>
             {category.sites.map((site) => (
               <li key={site.id} className="cursor-pointer">
-                {site.name}
+                <a onClick={() => selectSite && selectSite(site.id)}>
+                  {site.name}
+                </a>
               </li>
             ))}
           </ul>
