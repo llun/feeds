5aef92dfe6e0521fe6bb133efcfb402dc2fc12bf
Maythee Anegboonlap <null@llun.me>
2021-12-13 22:05:20 +0800

    Remove old feed files generator

--- action/feeds/index.js
+++ action/feeds/index.js
@@ -1,8 +1,6 @@
 // @ts-check
-const crypto = require('crypto')
 const core = require('@actions/core')
 const fs = require('fs')
-const path = require('path')
 const axios = require('axios').default
 const { parseXML, parseAtom, parseRss } = require('./parsers')
 const {
@@ -55,64 +53,6 @@ async function readOpml(opmlContent) {
   }, [])
 }
 exports.readOpml = readOpml
-
-/**
- *
- * @param {string} rootDirectory
- * @param {string} category
- */
-function createCategoryDirectory(rootDirectory, category) {
-  try {
-    const stats = fs.statSync(path.join(rootDirectory, category))
-    if (!stats.isDirectory()) {
-      throw new Error(
-        `${path.join(rootDirectory, category)} is not a directory`
-      )
-    }
-  } catch (error) {
-    if (error.code !== 'ENOENT') {
-      throw new Error(`Fail to access ${rootDirectory}`)
-    }
-    fs.mkdirSync(path.join(rootDirectory, category), { recursive: true })
-  }
-}
-exports.createCategoryDirectory = createCategoryDirectory
-
-async function writeFeedsContent() {
-  try {
-    const contentDirectory = core.getInput('outputDirectory', {
-      required: true
-    })
-    const feedsFile = core.getInput('opmlFile', { required: true })
-    const opmlContent = fs.readFileSync(feedsFile).toString('utf8')
-    const opml = await readOpml(opmlContent)
-    for (const category of opml) {
-      const { category: title, items } = category
-      createCategoryDirectory(contentDirectory, title)
-      if (!items) continue
-      console.log(`Load category ${title}`)
-      for (const item of items) {
-        const feedData = await loadFeed(item.title, item.xmlUrl)
-        if (!feedData) {
-          continue
-        }
-        console.log(`Load ${feedData.title}`)
-        const sha256 = crypto.createHash('sha256')
-        sha256.update(feedData.title)
-        const hexTitle = sha256.digest('hex')
-        fs.writeFileSync(
-          path.join(contentDirectory, title, `${hexTitle}.json`),
-          JSON.stringify(feedData)
-        )
-      }
-    }
-  } catch (error) {
-    console.error(error.message)
-    console.error(error.stack)
-    core.setFailed(error)
-  }
-}
-exports.writeFeedsContent = writeFeedsContent
 
 async function createFeedDatabase() {
   try {
