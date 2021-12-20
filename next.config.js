ecff98d3ca8c4c0517538ed432dcf3b8ecaa229c
Maythee Anegboonlap <null@llun.me>
2021-12-20 09:55:51 +0800

    Add basePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/:any*',
-        destination: '/'
-      }
-    ]
+const core = require('@actions/core')
+
+module.exports = (phase, { defaultConfig }) => {
+  const customDomainName = core.getInput('customDomain')
+  const githubRootName = process.env['GITHUB_REPOSITORY'] || ''
+  const basePath = customDomainName
+    ? '/'
+    : (githubRootName.split('/').length > 1 &&
+        `/${githubRootName.split('/')[1]}`) ||
+      '/'
+
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    basePath,
+    async rewrites() {
+      return [
+        {
+          source: '/:any*',
+          destination: '/'
+        }
+      ]
+    }
   }
+  return nextConfig
 }
