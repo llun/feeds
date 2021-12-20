f3f38d4c9191aaaf0d7fd19d888abf02fb1d3616
Maythee Anegboonlap <null@llun.me>
2021-12-20 21:26:27 +0800

    Copy old database back to repo

--- action/repository.js
+++ action/repository.js
@@ -38,6 +38,11 @@ exports.getGithubActionPath = getGithubActionPath
 function buildSite() {
   const workSpace = process.env['GITHUB_WORKSPACE']
   if (workSpace) {
+    // Copy old database to public
+    runCommand(
+      ['cp', `${workSpace}/data.sqlite3`, 'public/'],
+      getGithubActionPath()
+    )
     // Remove old static resources
     runCommand(['rm', '-rf', '_next'])
     // Bypass Jekyll
