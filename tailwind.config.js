7cba99e5569a4a8fffc1439ee03d55e78183fcab
Maythee Anegboonlap <null@llun.me>
2022-01-08 09:49:02 +0800

    Fix tailwind stop expanding ext for filename

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './lib/components/**/*.{js,ts,jsx,tsx}',
-    './lib/utils.{js,ts,jsx,tsx}'
+    './lib/utils.ts'
   ],
   plugins: [require('@tailwindcss/typography')]
 }
