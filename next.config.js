ce63326850fde877d12e5dbb72c594ad351d468c
Maythee Anegboonlap <contact@llun.dev>
2024-12-13 19:48:19 +0100

    Update NextJS config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
 const core = require('@actions/core')
 
-module.exports = (phase, { defaultConfig }) => {
-  const customDomainName = core.getInput('customDomain')
-  const githubRootName = process.env['GITHUB_REPOSITORY'] || ''
-  const basePath = customDomainName
-    ? ''
-    : (githubRootName.split('/').length > 1 &&
-        `/${githubRootName.split('/')[1]}`) ||
-      ''
+const customDomainName = core.getInput('customDomain')
+const githubRootName = process.env['GITHUB_REPOSITORY'] || ''
+const basePath = customDomainName
+  ? ''
+  : (githubRootName.split('/').length > 1 &&
+      `/${githubRootName.split('/')[1]}`) ||
+    ''
 
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    basePath,
-    async rewrites() {
-      return [
-        {
-          source: '/:any*',
-          destination: '/'
-        }
-      ]
-    }
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  basePath,
+  output: 'export',
+  async rewrites() {
+    return [
+      {
+        source: '/:any*',
+        destination: '/'
+      }
+    ]
   }
-  return nextConfig
 }
+return nextConfig
