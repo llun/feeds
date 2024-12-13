ebb242aa826a6abe30e9ba32de9660731b7cb649
Maythee Anegboonlap <contact@llun.dev>
2024-12-13 20:00:37 +0100

    Revert next config to import github core dynamically

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
-import core from '@actions/core'
 import { NextConfig } from 'next'
 
-const customDomainName = core.getInput('customDomain')
-const githubRootName = process.env['GITHUB_REPOSITORY'] || ''
-const basePath = customDomainName
-  ? ''
-  : (githubRootName.split('/').length > 1 &&
-      `/${githubRootName.split('/')[1]}`) ||
-    ''
-
-const nextConfig: NextConfig = {
-  basePath,
-  output: 'export'
+export default async () => {
+  const core = await import('@actions/core')
+  const customDomainName = core.getInput('customDomain')
+  const githubRootName = process.env['GITHUB_REPOSITORY'] || ''
+  const basePath = customDomainName
+    ? ''
+    : (githubRootName.split('/').length > 1 &&
+        `/${githubRootName.split('/')[1]}`) ||
+      ''
+  const nextConfig: NextConfig = {
+    basePath,
+    output: 'export'
+  }
+  return nextConfig
 }
-export default nextConfig
